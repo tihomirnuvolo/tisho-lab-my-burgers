e4bc8cd88a4590156fae085993c3b62f10a75989
Tihomir Todorov <tihomir.todorov@nuvolo.com>
2022-04-05 15:18:38 +0300

    Fix row selection bug.

--- src/components/Burgers/AllBurgers.tsx
+++ src/components/Burgers/AllBurgers.tsx
@@ -3,13 +3,11 @@ import { useDispatch, useSelector } from "react-redux";
 import { getBurgers } from "src/services/BurgerService";
 import { burgersState } from "src/store/burgersSlice";
 import { PreLoader } from "src/nuvolo/nuux/components/NuvoLoader";
-import { DataGrid } from "@components/common/DataGrid/DataGrid";
-import { BURGERS_COLUMN_CONFIG } from "@utils/constants";
 import {
   Column,
+  NuvoDataGrid,
   Pager,
   Paging,
-  Sorting,
 } from "@nuvolo/nuux/components/NuvoDataGrid";
 import { ColumnParams } from "src/types/columnParams";
 import { Burger } from "src/types/Burger";
@@ -35,26 +33,7 @@ export const AllBurgers = (): JSX.Element => {
     dispatch(getBurgers());
   }, []);
 
-  const defaultDataGridOptions = {
-    column: {
-      allowResizing: false,
-      allowReordering: false,
-    },
-    sorting: {
-      sortingMode: "multiple",
-    },
-  };
-
   const columnMap: Array<ColumnParams> = [
-    // {
-    //   columnName: "sorting_date",
-    //   columnLabel: "Payment Date",
-    //   translate_key: "RE_PAYMENT_LIST_COLUMN_PAYMENT_DATE",
-    //   columnType: "date",
-    //   groupIndex: 0,
-    //   visible: false,
-    //   showInColumnChooser: false,
-    // },
     {
       columnName: "sys_id",
       columnLabel: "Id",
@@ -121,29 +100,15 @@ export const AllBurgers = (): JSX.Element => {
               </Row>
             </Container>
           </div>
-          <DataGrid
+          <NuvoDataGrid
             keyExpr="sys_id"
             dataSource={burgers}
+            showBorders
             showColumnLines
             showRowLines
             columnAutoWidth
-            allowColumnReordering
             onRowClick={onRowClick}
-            // selection={{
-            //   mode: "none",
-            //   deferred: true,
-            //   showCheckBoxesMode: "none",
-            // }}
-            configName={BURGERS_COLUMN_CONFIG}
-            height="100%"
           >
-            <Sorting
-              mode={
-                defaultDataGridOptions.sorting.sortingMode
-                  ? defaultDataGridOptions.sorting.sortingMode
-                  : "none"
-              }
-            />
             {columnMap.map((c, index) => (
               <Column
                 // eslint-disable-next-line react/no-array-index-key
@@ -152,16 +117,12 @@ export const AllBurgers = (): JSX.Element => {
                 dataField={c.columnName}
                 dataType={c.columnType}
                 visible={c.visible}
-                // format={c.format}
-                // cssClass={c.cssClass}
-                // alignment={c.alignment}
-                // width={c.width}
                 headerCellRender={renderTitleHeader}
               />
             ))}
             <Paging defaultPageSize={10} />
             <Pager showPageSizeSelector allowedPageSizes={[10, 20, 50, 100]} />
-          </DataGrid>
+          </NuvoDataGrid>
           <CreateBurger
             open={createBurger}
             setOpen={setCreateBurger}
